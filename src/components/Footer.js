d1ac044d42cd75524fe70d73406d08bf58170d34
BarnBuilder412 <nipunshah1729@gmail.com>
2024-11-08 17:09:44 +0530

    new logo added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2024 Trigg3rX. All rights reserved.</p>
+        <p>&copy; 2024 TriggerX. All rights reserved.</p>
       </div>
     </footer>
   );
