b145ad9a5e3f307da24aa46ad4edd8a81b084517
BarnBuilder412 <nipunshah1729@gmail.com>
2025-04-21 14:25:43 +0530

    fix:remove previous implemantation of proxy contract

--- src/pages/CreateJobPage/index.js
+++ src/pages/CreateJobPage/index.js
@@ -24,7 +24,6 @@ import eventBasedGif from "../../assets/event-based.gif";
 
 import DeleteConfirmationButton from "./components/DeleteConfirmationButton";
 import { WarningOutlined } from "@ant-design/icons";
-import { useProxyFactory } from "./hooks/useProxyFactory";
 import { randomBytes } from "ethers";
 
 const networkIcons = {
@@ -133,7 +132,6 @@ function CreateJobPage() {
   const { handleKeyDown } = useFormKeyboardNavigation();
   const [contractDetails, setContractDetails] = useState({});
   const [recurring, setRecurring] = useState(false);
-  const { deployNewProxy, isDeploying } = useProxyFactory();
   const baseUrl = 'https://app.triggerx.network';
 
   useEffect(() => {
@@ -428,12 +426,6 @@ function CreateJobPage() {
       const allJobsDetails = [];
       const mainJobDetails = contractDetails[jobType]?.["main"];
 
-      // Deploy proxy for main job
-      const { proxyAddress: mainProxyAddress } = await deployNewProxy(
-        mainJobDetails.contractAddress,
-        address
-      );
-
       // Calculate taskdefinitionid and argType based on job type and argument type
       const getTaskDefinitionId = (argumentType) => {
         return argumentType === "static"
@@ -469,7 +461,6 @@ function CreateJobPage() {
           recurring: recurring,
           trigger_chain_id: triggerChainId.toString(),
           trigger_contract_address: mainJobDetails.contractAddress,
-          abstraction_contract: mainProxyAddress,
           trigger_event: "NULL",
           script_ipfs_url: mainJobDetails.ipfsCodeUrl || "",
           script_target_function: "trigger",
@@ -489,15 +480,6 @@ function CreateJobPage() {
         for (const jobId of linkedJobs[jobType]) {
           const linkedJobDetails = contractDetails[jobType]?.[jobId];
 
-          let linkedProxyAddress = mainProxyAddress;
-          if(mainJobDetails.contractAddress !== linkedJobDetails.contractAddress){
-            // Deploy proxy for linked job
-            const { proxyAddress: linkedProxyAddress } = await deployNewProxy(
-              linkedJobDetails.contractAddress,
-              address
-            );
-          }
-
           if (linkedJobDetails) {
             const taskdefinitionid = getTaskDefinitionId(linkedJobDetails.argumentType);
             const argType = getArgType(linkedJobDetails.argumentType);
@@ -515,7 +497,6 @@ function CreateJobPage() {
               recurring: false,
               trigger_chain_id: triggerChainId.toString(),
               trigger_contract_address: linkedJobDetails.contractAddress,
-              abstraction_contract: linkedProxyAddress,
               trigger_event: "NULL",
               script_ipfs_url: linkedJobDetails.ipfsCodeUrl || "",
               script_target_function: "trigger",
